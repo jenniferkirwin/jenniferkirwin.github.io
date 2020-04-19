f31d31dbfb972839eb928cde730768fa20635f81
jenniferkirwin <lamontagneartist@gmail.com>
2020-04-19 12:57:52 -0400

    Cleaned up code

--- js/scripts.js
+++ js/scripts.js
@@ -2,47 +2,6 @@
 // Portfolio JSON Object
 // ---------------------------------------------------------------------------
 
-// console.log(portfolio);
-
-// const portfolio = [
-//     {
-//         "projectName":"Password Generator",
-//         "projectOver":"Creates a password with lowercase, uppercase, numeric, and special characters.",
-//         "projectDescr":"Creates a password with lowercase, uppercase, numeric, and special characters.",
-//         "projectURL":"https://jenniferkirwin.github.io/password-generator2/",
-//         "githubURL":"https://github.com/jenniferkirwin/password-generator2",
-//         "imgSrc":"images/pass-gen.jpg",
-//         "builtWith":"html css"
-//     },
-//     {
-//         "projectName":"JavaScript Quiz",
-//         "projectOver":"A short 4 question timed quiz testing JavaScript fundamentals and allows the user to store their score to local storage.",
-//         "projectDescr":"A short 4 question timed quiz testing JavaScript fundamentals and allows the user to store their score to local storage.",
-//         "projectURL":"https://jenniferkirwin.github.io/code-quiz/",
-//         "githubURL":"https://github.com/jenniferkirwin/code-quiz",
-//         "imgSrc":"images/quiz.jpg",
-//         "builtWith":"html css javascript jquery"
-//     },
-//     {
-//         "projectName":"Day Planner",
-//         "projectOver":"A simple day planner that takes user input and stores it into local storage.",
-//         "projectDescr":"A simple day planner that takes user input and stores it into local storage.",
-//         "projectURL":"https://jenniferkirwin.github.io/scheduling-app/",
-//         "githubURL":"https://github.com/jenniferkirwin/scheduling-app",
-//         "imgSrc":"images/scheduler.jpg",
-//         "builtWith":"html css javascript jquery"
-//     },
-//     {
-//         "projectName":"SuperEats",
-//         "projectOver":"An application to help you meal plan for any holiday. Developed as a group, with myself as team lead.",
-//         "projectDescr":"An application to help you meal plan for any holiday. Developed as a group, with myself as team lead.",
-//         "projectURL":"https://nwsanfor24.github.io/holiday-meal-planner//",
-//         "githubURL":"https://github.com/nwsanfor24/holiday-meal-planner",
-//         "imgSrc":"images/supereats.jpg",
-//         "builtWith":"html css javascript jquery"
-//     },
-// ]
-
 $(document).ready(function () {
 
   const $overlayDiv = $(`.overlay-div`);
@@ -61,8 +20,6 @@ $(document).ready(function () {
   // Populating each project card
 
   $.getJSON("js/portfolio.json", function (data) {
-
-    console.log(data);
 
     data.forEach(function (item, index) {
 
@@ -84,25 +41,6 @@ $(document).ready(function () {
     });
 
   });
-
-  // portfolio.forEach(function(item, index) {
-
-  //     $(`.portfolio-card-container`).append(
-  //         `<div class="filterShow ${item.builtWith}">
-  //             <div class="col s12 m3 l4">
-  //                 <div class="card portfolio-margin project-card" data-project="${index}">
-  //                     <div class="card-image waves-effect waves-block waves-light">
-  //                     <img src="${item.imgSrc}" alt="${item.projectName}">
-  //                     </div>
-  //                     <div class="card-content">
-  //                     <span class="card-title grey-text text-darken-4">${item.projectName} <i
-  //                         class="fas fa-arrow-alt-circle-right"></i></span>
-  //                     </div>
-  //                 </div>
-  //             </div>
-  //         </div>`
-  //     )
-  // });
 
   // Hiding & showing portfolio div
 
