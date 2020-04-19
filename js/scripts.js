d87a0adeb393bc65f16aa40ef80ff5296f7c0fc9
jenniferkirwin <lamontagneartist@gmail.com>
2020-04-19 12:55:10 -0400

    Adding in rendering portfolio card

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,6 @@
 // Portfolio JSON Object
 // ---------------------------------------------------------------------------
 
-const portfolio = "portfolio.json";
-
-$.getJSON( "js/portfolio.json", function( data ) {
-  const test = data;
-  console.log(data);
-})
-
 // console.log(portfolio);
 
 // const portfolio = [
@@ -50,102 +43,127 @@ $.getJSON( "js/portfolio.json", function( data ) {
 //     },
 // ]
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    const $overlayDiv = $(`.overlay-div`);
-    const $overlayX = $(`.overlay-x`);
+  const $overlayDiv = $(`.overlay-div`);
+  const $overlayX = $(`.overlay-x`);
 
-    const $overlayImg = $(`.overlay-img`);
-    const $overlayTitle = $(`.overlay-title`);
-    const $overlayDescr = $(`.overlay-descr`);
-    const $projectURL = $(`.project-url`);
-    const $githubURL = $(`.github-url`);
+  const $overlayImg = $(`.overlay-img`);
+  const $overlayTitle = $(`.overlay-title`);
+  const $overlayDescr = $(`.overlay-descr`);
+  const $projectURL = $(`.project-url`);
+  const $githubURL = $(`.github-url`);
 
-// ---------------------------------------------------------------------------
-// Handling Portfolio on Website
-// ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  // Handling Portfolio on Website
+  // ---------------------------------------------------------------------------
 
-// Populating each project card
+  // Populating each project card
 
-// portfolio.forEach(function(item, index) {
+  $.getJSON("js/portfolio.json", function (data) {
 
-//     $(`.portfolio-card-container`).append(
-//         `<div class="filterShow ${item.builtWith}">
-//             <div class="col s12 m3 l4">
-//                 <div class="card portfolio-margin project-card" data-project="${index}">
-//                     <div class="card-image waves-effect waves-block waves-light">
-//                     <img src="${item.imgSrc}" alt="${item.projectName}">
-//                     </div>
-//                     <div class="card-content">
-//                     <span class="card-title grey-text text-darken-4">${item.projectName} <i
-//                         class="fas fa-arrow-alt-circle-right"></i></span>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>`
-//     )
-// });
+    console.log(data);
 
-// Hiding & showing portfolio div
+    data.forEach(function (item, index) {
 
-    $(`.project-card`).on(`click`, function() {
-        console.log($(this).attr(`data-project`))
-        const projectData =  portfolio[$(this).attr(`data-project`)];
-        $overlayTitle.text(projectData.projectName);
-        $overlayDescr.text(projectData.projectDescr);
-        $projectURL.attr(`href`, projectData.projectURL);
-        $githubURL.attr(`href`, projectData.githubURL);
-        $overlayImg.attr(`src`, projectData.imgSrc);
-        $overlayDiv.removeClass(`fadeOutUp hidden-div`).addClass(`fadeInDown`);
+      $(`.portfolio-card-container`).append(
+        `<div class="filterShow ${item.builtWith}">
+            <div class="col s12 m3 l4">
+                <div class="card portfolio-margin project-card" data-project="${index}">
+                    <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${item.imgSrc}" alt="${item.projectName}">
+                    </div>
+                    <div class="card-content">
+                    <span class="card-title grey-text text-darken-4">${item.projectName} <i
+                        class="fas fa-arrow-alt-circle-right"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>`
+      )
     });
 
-    $overlayX.on(`click`, function() {
-        $overlayDiv.removeClass(`fadeOutUp`).addClass(`fadeOutUp`);
-    });
+  });
+
+  // portfolio.forEach(function(item, index) {
+
+  //     $(`.portfolio-card-container`).append(
+  //         `<div class="filterShow ${item.builtWith}">
+  //             <div class="col s12 m3 l4">
+  //                 <div class="card portfolio-margin project-card" data-project="${index}">
+  //                     <div class="card-image waves-effect waves-block waves-light">
+  //                     <img src="${item.imgSrc}" alt="${item.projectName}">
+  //                     </div>
+  //                     <div class="card-content">
+  //                     <span class="card-title grey-text text-darken-4">${item.projectName} <i
+  //                         class="fas fa-arrow-alt-circle-right"></i></span>
+  //                     </div>
+  //                 </div>
+  //             </div>
+  //         </div>`
+  //     )
+  // });
+
+  // Hiding & showing portfolio div
+
+  $(`.project-card`).on(`click`, function () {
+    console.log($(this).attr(`data-project`))
+    const projectData = portfolio[$(this).attr(`data-project`)];
+    $overlayTitle.text(projectData.projectName);
+    $overlayDescr.text(projectData.projectDescr);
+    $projectURL.attr(`href`, projectData.projectURL);
+    $githubURL.attr(`href`, projectData.githubURL);
+    $overlayImg.attr(`src`, projectData.imgSrc);
+    $overlayDiv.removeClass(`fadeOutUp hidden-div`).addClass(`fadeInDown`);
+  });
+
+  $overlayX.on(`click`, function () {
+    $overlayDiv.removeClass(`fadeOutUp`).addClass(`fadeOutUp`);
+  });
 
 
-// ---------------------------------------------------------------------------
-// portfolio filter
-// ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  // portfolio filter
+  // ---------------------------------------------------------------------------
 
 
-    // adding .filter-show and removing .filter-show on div
-    // filterSelection("all")
-    // function filterSelection(c) {
-    //     var x, i;
-    //     x = document.getElementsByClassName("filterHide");
-    //     if (c == "all") c = "";
-    //     for (i = 0; i < x.length; i++) {
-    //         removeClass(x[i], "filterShow");
-    //         if (x[i].className.indexOf(c) > -1) addClass (x[i], "filterShow")
-    //     }
-    // }
+  // adding .filter-show and removing .filter-show on div
+  // filterSelection("all")
+  // function filterSelection(c) {
+  //     var x, i;
+  //     x = document.getElementsByClassName("filterHide");
+  //     if (c == "all") c = "";
+  //     for (i = 0; i < x.length; i++) {
+  //         removeClass(x[i], "filterShow");
+  //         if (x[i].className.indexOf(c) > -1) addClass (x[i], "filterShow")
+  //     }
+  // }
 
-    // // function to show filtered divs
+  // // function to show filtered divs
 
-    // function addClass(element, name) {
-    //     var i, arr1, arr2;
-    //     arr1 = element.className.split(" ");
-    //     arr2 = name.split(" ");
-    //     for (i = 0; i < arr2.length; i++) {
-    //         if (arr1.indexOf(arr2[i]) == -1) {
-    //             element.className += " " + arr2[i];
-    //         }
-    //     }
-    // }
+  // function addClass(element, name) {
+  //     var i, arr1, arr2;
+  //     arr1 = element.className.split(" ");
+  //     arr2 = name.split(" ");
+  //     for (i = 0; i < arr2.length; i++) {
+  //         if (arr1.indexOf(arr2[i]) == -1) {
+  //             element.className += " " + arr2[i];
+  //         }
+  //     }
+  // }
 
-    // // function to hide remaining divs
+  // // function to hide remaining divs
 
-    // function removeClass(element, name) {
-    //     var i, arr1, arr2;
-    //     arr1 = element.className.split(" ");
-    //     arr2 = name.split(" ");
-    //     for (i = 0; i < arr2.length; i++) {
-    //         while (arr1.indexOf(arr2[i]) > -1) {
-    //             arr1.splice(arr1.indexOf(arr2[i]), 1);
-    //         }
-    //     }
-    //     element.className = arr1.join(" ");
-    // }
+  // function removeClass(element, name) {
+  //     var i, arr1, arr2;
+  //     arr1 = element.className.split(" ");
+  //     arr2 = name.split(" ");
+  //     for (i = 0; i < arr2.length; i++) {
+  //         while (arr1.indexOf(arr2[i]) > -1) {
+  //             arr1.splice(arr1.indexOf(arr2[i]), 1);
+  //         }
+  //     }
+  //     element.className = arr1.join(" ");
+  // }
 
 });
